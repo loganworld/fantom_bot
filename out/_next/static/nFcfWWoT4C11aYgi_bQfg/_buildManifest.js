991687a968d2d4f04932599ec8f8928745890e04
loganworld <akp707d@gmail.com>
2021-07-23 21:30:37 +1000

    1.1

---
+++ out/_next/static/nFcfWWoT4C11aYgi_bQfg/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/commons-b9f5c4f93d3782d2883f.js","static/chunks/678-f0bc5b3862808b65b2e1.js","static/chunks/pages/index-a626058177b354f205ec.js"],"/_error":["static/chunks/pages/_error-f5e67a79c709c2df06b8.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
